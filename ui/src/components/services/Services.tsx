0bda1ff09d02b0aa72b692a2ca57a389b6627cd1
Rich Mischler <rich.mischler@blackboard.com>
2022-05-16 11:44:10 -0400

    feat: add another service, reduce width

--- ui/src/components/services/Services.tsx
+++ ui/src/components/services/Services.tsx
@@ -13,6 +13,8 @@ import { useNavigate } from "react-router-dom";
 import services from "../../resources/services.json";
 // @ts-ignore
 import site from "../../resources/site.jpg";
+// @ts-ignore
+import auto from "../../resources/auto.jpg";
 
 export interface ServicesProps {
   readonly active?: boolean | undefined;
@@ -25,15 +27,15 @@ export const useStyles = makeStyles((theme) => ({
   card: {
     marginTop: theme.spacing(1),
     padding: theme.spacing(1, 1, 1, 1),
-    maxWidth: "100vh",
-    minWidth: 350,
+    maxWidth: 320,
+    minWidth: 320,
   },
   media: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    maxHeight: "100vh",
+    maxHeight: 250,
     minHeight: 250,
   },
   title: {
@@ -62,33 +64,63 @@ export const Services: FunctionComponent<ServicesProps> = ({ active }) => {
   return (
     <Grid
       container
-      spacing={0}
-      direction="column"
+      spacing={4}
+      direction="row"
       alignItems="center"
       justifyContent="center"
     >
-      <Card className={classes.card}>
-        <CardMedia className={classes.media}>
-          <img src={site} alt={"site"} />
-        </CardMedia>
-        <CardContent>
-          <Typography className={classes.title}>{services.ui.title}</Typography>
-          <Typography className={classes.desc}>
-            {" "}
-            {services.ui.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button
-            className={classes.button}
-            variant={"outlined"}
-            onClick={handleClick}
-            size="small"
-          >
-            Contact Me
-          </Button>
-        </CardActions>
-      </Card>
+      <Grid item>
+        <Card className={classes.card}>
+          <CardMedia className={classes.media}>
+            <img src={site} alt={"site"} />
+          </CardMedia>
+          <CardContent>
+            <Typography className={classes.title}>
+              {services.ui.title} - {services.ui.price}
+            </Typography>
+            <Typography className={classes.desc}>
+              {" "}
+              {services.ui.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              className={classes.button}
+              variant={"outlined"}
+              onClick={handleClick}
+              size="small"
+            >
+              Contact Me
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card className={classes.card}>
+          <CardMedia className={classes.media}>
+            <img src={auto} alt={"auto"} />
+          </CardMedia>
+          <CardContent>
+            <Typography className={classes.title}>
+              {services.auto.title} - {services.auto.price}
+            </Typography>
+            <Typography className={classes.desc}>
+              {" "}
+              {services.auto.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              className={classes.button}
+              variant={"outlined"}
+              onClick={handleClick}
+              size="small"
+            >
+              Contact Me
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
     </Grid>
   );
 };
